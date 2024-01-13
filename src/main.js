43d9a1ba39f3b26e0a966838a1e8782cbe963408
Bruno <brunoamarante83@gmail.com>
2024-01-12 22:29:57 -0300

    Criando o efeito no cabeçalho

--- src/main.js
+++ src/main.js
@@ -1,7 +1,26 @@
 document.addEventListener('DOMContentLoaded', function() {
     const buttons = document.querySelectorAll('[data-tab-button]');
+    const questions = document.querySelectorAll('[data-faq-question]');
+    
+    const herosection = document.querySelector('.hero');
+    const alturaHero = herosection.clientHeigth;
+
+    
+    window.addEventListener('scroll', function() {
+        const posicaoatual = window.scrollY;
+        
+        if (posicaoatual < alturaHero) {
+            ocultaelementosdoheader();
+        } else {
+            exibeelementosdoheader();
+        }
+        
+    })
+    
     
 
+    
+    // Seção de atrações programação das abas
     for (let i = 0; i < buttons.length; i++) {
         buttons[i].addEventListener('click', function(botao) {
             const abaalvo = botao.target.dataset.tabButton;
@@ -12,7 +31,31 @@ document.addEventListener('DOMContentLoaded', function() {
             botao.target.classList.add('shows__tabs__button--is--active');
         })
     }
+
+    // Seção faq, Acordion
+    for (let i = 0; i < questions.length; i++) {
+        questions[i].addEventListener('click', abreoufecharespostas);
+    }
 })
+
+    // Seção ocultando header
+    function ocultaelementosdoheader() {
+        const header = document.querySelector('header');
+        header.classList.add('header--is-hidden');
+    }
+    
+    function exibeelementosdoheader() {
+        const header = document.querySelector('header');
+        header.classList.remove('header--is-hidden');
+    }
+
+
+function abreoufecharespostas(elemento) {
+    const classe = 'faq__questions__item--is-open';
+    const elementopai = elemento.target.parentNode;
+
+    elementopai.classList.toggle(classe);
+}
 
 function removebotaoativo() {
     const buttons = document.querySelectorAll('[data-tab-button]');
